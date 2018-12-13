5c3957764cc00ed43b2ca13a9bdfeb25e13b43a1
HeithemD <heithemdridi@protonmail.com>
2018-12-13 10:55:16 +0100

    function Robot

---
+++ JS/robot.js
@@ -0,0 +1,8 @@
+function Robot(x, y, vitesse, couleur){
+    this.posX    = x;
+    this.posY    = y;
+    this.temps   = vitesse;
+    this.couleur = couleur;
+    this.timeID  = NULL;
+    this.eveille = false;
+}
