ead4cbca60dccd2f990a52dbf289fad4e496d62d
HeithemD <heithemdridi@protonmail.com>
2018-12-13 11:44:29 +0100

    Modify prototype

--- JS/robot.js
+++ JS/robot.js
@@ -1,22 +1,38 @@
-function robot(x, y, vitesse, couleur) {
-    this.posX                = x;
-    this.posY                = y;
-    this.temps               = vitesse;
-    this.couleur             = couleur;
-    this.timeID              = NULL;
-    this.eveille             = false;
-    this.dessin(ctx, dx, dy) = function(){
+function Robot(x, y, vitesse, couleur) {
+    this.posX    = x;
+    this.posY    = y;
+    this.temps   = vitesse;
+    this.couleur = couleur;
+    this.timeID  = NULL;
+    this.eveille = false;
+}
+
+Grille.prototype.dessin(ctx, dx, dy) = function(){
     };
-    this.perception()        = function(){
+
+Grille.prototype.perception()      = function(){
+        tabMvt = [];
+        if (gr.mvtAdmis(robot, 0,1)) {
+            tabMvt.push("Up")
+        }
+        if (gr.mvtAdmis(robot, 0,-1)) {
+            tabMvt.push("Down")
+        }
+        if (gr.mvtAdmis(robot, 1,0)) {
+            tabMvt.push("Right")
+        }
+        if (gr.mvtAdmis(robot, -1,0)) {
+            tabMvt.push("Left")
+        }
+
+        return tabMvt;
+    }
+Grille.prototype.reflexion(tabMvt) = function(){
 
     }
-    this.reflexion(tabMvt)   = function(){
+Grille.prototype.action(act)       = function(){
 
     }
-    this.action(act)         = function(){
-
-    }
-    this.reveille()          = function(){
+Grille.prototype.reveille()        = function(){
         
     }
-}
