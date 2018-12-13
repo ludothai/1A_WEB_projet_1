f812637fcbe26b11f62eb6ebef984a1d7e8cd389
HeithemD <heithemdridi@protonmail.com>
2018-12-13 11:16:16 +0100

    Déclaration des méthode

--- JS/robot.js
+++ JS/robot.js
@@ -1,8 +1,22 @@
-function Robot(x, y, vitesse, couleur){
-    this.posX    = x;
-    this.posY    = y;
-    this.temps   = vitesse;
-    this.couleur = couleur;
-    this.timeID  = NULL;
-    this.eveille = false;
+function robot(x, y, vitesse, couleur) {
+    this.posX                = x;
+    this.posY                = y;
+    this.temps               = vitesse;
+    this.couleur             = couleur;
+    this.timeID              = NULL;
+    this.eveille             = false;
+    this.dessin(ctx, dx, dy) = function(){
+    };
+    this.perception()        = function(){
+
+    }
+    this.reflexion(tabMvt)   = function(){
+
+    }
+    this.action(act)         = function(){
+
+    }
+    this.reveille()          = function(){
+        
+    }
 }
