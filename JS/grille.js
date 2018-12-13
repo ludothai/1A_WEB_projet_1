35213bd527f636f6d7bf23a09014167ab9ddb600
Ludo <ludo@github.com>
2018-12-13 10:56:32 +0100

    up

---
+++ JS/grille.js
@@ -0,0 +1,22 @@
+/* Définition d'une fonction constructeur de nom Grille  */
+function Grille(nbX, nbY, canvaID)
+{
+  /* Attributs de l'objet */
+  this.nbX = nbX;
+  this.nbY = nbY;
+  this.canva = canvaID;
+  this.ctx = ;
+  this.dX = 350/nbX;
+  this.dY = 350/nbY;
+  this.tab = [][];
+  this.obj = [];
+  this.nbC = (nbX)*(nbY);
+  this.nbCur = 0;
+
+  this.ajoutObjet = function()
+  {
+        
+  }
+
+
+}
