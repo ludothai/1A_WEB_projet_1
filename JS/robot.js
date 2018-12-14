07085908d9c025b7e998fedacf4e1eea767efe8f
HeithemD <heithemdridi@protonmail.com>
2018-12-14 09:08:59 +0100

    remove timeID =NULL

--- JS/robot.js
+++ JS/robot.js
@@ -5,7 +5,7 @@ function Robot(x, y, vitesse, couleur) {
     this.posY    = y;
     this.temps   = vitesse;
     this.couleur = couleur;
-    this.timeID  = NULL;
+    this.timeID;
     this.eveille = false;
 }
 
